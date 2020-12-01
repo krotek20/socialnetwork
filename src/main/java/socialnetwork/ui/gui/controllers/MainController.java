77eaf95db9f077066c6c28f9459ea553b097f65f
TufisStefan <stefan_lasto@yahoo.com>
2020-12-01 11:44:26 +0200

    added tab pane

--- src/main/java/socialnetwork/ui/gui/controllers/MainController.java
+++ src/main/java/socialnetwork/ui/gui/controllers/MainController.java
@@ -1,4 +1,20 @@
 package socialnetwork.ui.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
 public class MainController {
+    @FXML
+    private Tab account;
+
+    @FXML
+    private Tab notifications;
+
+    @FXML
+    private Tab chats;
+
+    @FXML
+    public void initialize(){}
+
+
 }
